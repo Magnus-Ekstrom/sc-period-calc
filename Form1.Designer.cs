8009ce32364a027dba31bacf0dc94e15e6ad6495
Magnus Ekström <magnus.ekstrom_c@sandvik.com>
2025-02-14 09:32:15 +0100

    Initial checkin

---
+++ Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace period_calc
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            cbHolidaysUsed = new CheckBox();
+            lblHolidaysUsed = new Label();
+            lbHolidaysUsed = new ListBox();
+            lbPeriods = new ListBox();
+            cbShowWeekends = new CheckBox();
+            numYear = new NumericUpDown();
+            rtpOutput = new RichTextBox();
+            tabPage2 = new TabPage();
+            btnReadHolidays = new Button();
+            label1 = new Label();
+            tbHolydays = new TextBox();
+            btnReset = new Button();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numYear).BeginInit();
+            tabPage2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Dock = DockStyle.Fill;
+            tabControl1.Location = new Point(0, 0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(638, 608);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(btnReset);
+            tabPage1.Controls.Add(cbHolidaysUsed);
+            tabPage1.Controls.Add(lblHolidaysUsed);
+            tabPage1.Controls.Add(lbHolidaysUsed);
+            tabPage1.Controls.Add(lbPeriods);
+            tabPage1.Controls.Add(cbShowWeekends);
+            tabPage1.Controls.Add(numYear);
+            tabPage1.Controls.Add(rtpOutput);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(630, 580);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Beräkna";
+            // 
+            // cbHolidaysUsed
+            // 
+            cbHolidaysUsed.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            cbHolidaysUsed.AutoSize = true;
+            cbHolidaysUsed.Checked = true;
+            cbHolidaysUsed.CheckState = CheckState.Checked;
+            cbHolidaysUsed.Location = new Point(454, 298);
+            cbHolidaysUsed.Name = "cbHolidaysUsed";
+            cbHolidaysUsed.Size = new Size(129, 19);
+            cbHolidaysUsed.TabIndex = 13;
+            cbHolidaysUsed.Text = "Use holidays in calc";
+            cbHolidaysUsed.UseVisualStyleBackColor = true;
+            // 
+            // lblHolidaysUsed
+            // 
+            lblHolidaysUsed.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            lblHolidaysUsed.AutoSize = true;
+            lblHolidaysUsed.Location = new Point(454, 321);
+            lblHolidaysUsed.Name = "lblHolidaysUsed";
+            lblHolidaysUsed.Size = new Size(82, 15);
+            lblHolidaysUsed.TabIndex = 12;
+            lblHolidaysUsed.Text = "Holidays Used";
+            // 
+            // lbHolidaysUsed
+            // 
+            lbHolidaysUsed.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            lbHolidaysUsed.FormattingEnabled = true;
+            lbHolidaysUsed.ItemHeight = 15;
+            lbHolidaysUsed.Location = new Point(454, 339);
+            lbHolidaysUsed.Name = "lbHolidaysUsed";
+            lbHolidaysUsed.Size = new Size(176, 229);
+            lbHolidaysUsed.TabIndex = 11;
+            // 
+            // lbPeriods
+            // 
+            lbPeriods.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            lbPeriods.FormattingEnabled = true;
+            lbPeriods.ItemHeight = 15;
+            lbPeriods.Location = new Point(454, 38);
+            lbPeriods.Name = "lbPeriods";
+            lbPeriods.Size = new Size(176, 229);
+            lbPeriods.TabIndex = 10;
+            lbPeriods.SelectedIndexChanged += lbPeriods_SelectedIndexChanged;
+            // 
+            // cbShowWeekends
+            // 
+            cbShowWeekends.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            cbShowWeekends.AutoSize = true;
+            cbShowWeekends.Location = new Point(454, 273);
+            cbShowWeekends.Name = "cbShowWeekends";
+            cbShowWeekends.Size = new Size(110, 19);
+            cbShowWeekends.TabIndex = 9;
+            cbShowWeekends.Text = "Show weekends";
+            cbShowWeekends.UseVisualStyleBackColor = true;
+            // 
+            // numYear
+            // 
+            numYear.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            numYear.Location = new Point(457, 9);
+            numYear.Maximum = new decimal(new int[] { 3000, 0, 0, 0 });
+            numYear.Name = "numYear";
+            numYear.Size = new Size(60, 23);
+            numYear.TabIndex = 8;
+            numYear.Value = new decimal(new int[] { 2025, 0, 0, 0 });
+            numYear.ValueChanged += numYear_ValueChanged;
+            // 
+            // rtpOutput
+            // 
+            rtpOutput.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            rtpOutput.Location = new Point(0, 0);
+            rtpOutput.Name = "rtpOutput";
+            rtpOutput.Size = new Size(451, 580);
+            rtpOutput.TabIndex = 4;
+            rtpOutput.Text = "";
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(btnReadHolidays);
+            tabPage2.Controls.Add(label1);
+            tabPage2.Controls.Add(tbHolydays);
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(630, 580);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Lediga dagar";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // btnReadHolidays
+            // 
+            btnReadHolidays.Location = new Point(363, 3);
+            btnReadHolidays.Name = "btnReadHolidays";
+            btnReadHolidays.Size = new Size(126, 23);
+            btnReadHolidays.TabIndex = 2;
+            btnReadHolidays.Text = "Läs in lediga dagar";
+            btnReadHolidays.UseVisualStyleBackColor = true;
+            btnReadHolidays.Click += btnReadHolidays_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(6, 7);
+            label1.Name = "label1";
+            label1.Size = new Size(338, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Lediga dagar. En dag per rad yyyy-mm-dd <short description>";
+            // 
+            // tbHolydays
+            // 
+            tbHolydays.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            tbHolydays.Location = new Point(0, 27);
+            tbHolydays.Multiline = true;
+            tbHolydays.Name = "tbHolydays";
+            tbHolydays.Size = new Size(630, 553);
+            tbHolydays.TabIndex = 0;
+            // 
+            // btnReset
+            // 
+            btnReset.Location = new Point(552, 9);
+            btnReset.Name = "btnReset";
+            btnReset.Size = new Size(75, 23);
+            btnReset.TabIndex = 14;
+            btnReset.Text = "Reset";
+            btnReset.UseVisualStyleBackColor = true;
+            btnReset.Click += btnReset_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(638, 608);
+            Controls.Add(tabControl1);
+            Name = "Form1";
+            Text = "SteelTech Time Reporting Calculator";
+            Load += Form1_Load;
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)numYear).EndInit();
+            tabPage2.ResumeLayout(false);
+            tabPage2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private TextBox tbHolydays;
+        private Label label1;
+        private RichTextBox rtpOutput;
+        private NumericUpDown numYear;
+        private CheckBox cbShowWeekends;
+        private ListBox lbPeriods;
+        private Button btnReadHolidays;
+        private Label lblHolidaysUsed;
+        private ListBox lbHolidaysUsed;
+        private CheckBox cbHolidaysUsed;
+        private Button btnReset;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,214 @@
+using System.Diagnostics;
+using System.Globalization;
+
+namespace period_calc
+{
+    public partial class Form1 : Form
+    {
+        private List<Holiday> currentHolidays = new List<Holiday>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            numYear.Value = DateTime.Now.Year;
+            LoadHolidaysFromFile();
+            ReadHolidays();
+
+            var periods = GeneratePeriods((int)numYear.Value);
+            var total = 0;
+            foreach (var period in periods)
+            {
+                var periodTotal = CalculateForPeriod(period.From, period.To, false);
+                rtpOutput.AppendText($"{period.From} - {period.To} : {periodTotal}\n");
+                total += periodTotal;
+            }
+            rtpOutput.AppendText($"Yearly total: {total}\n");
+
+            lbPeriods.Items.Clear();
+            lbPeriods.DisplayMember = "Display";
+            lbPeriods.Items.AddRange(periods.ToArray());
+
+        }
+
+        private void numYear_ValueChanged(object sender, EventArgs e)
+        {
+            rtpOutput.Clear();
+            var periods = GeneratePeriods((int)numYear.Value);
+            foreach (var period in periods)
+            {
+                var totalHours = CalculateForPeriod(period.From, period.To, false);
+                rtpOutput.AppendText($"{period.From} - {period.To} : {totalHours}\n");
+            }
+        }
+
+        private void lbPeriods_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var item = lbPeriods.SelectedItem as DateRange;
+            CalculateForPeriod(item.From, item.To);
+
+        }
+        private void btnReadHolidays_Click(object sender, EventArgs e)
+        {
+            ReadHolidays();
+        }
+
+        private void ReadHolidays()
+        {
+            currentHolidays.Clear();
+            foreach (string line in tbHolydays.Lines.Where(l => !string.IsNullOrWhiteSpace(l)))
+            {
+                var p = line.Split(' ', 2);
+                if (p.Length == 2)
+                {
+                    if (DateOnly.TryParse(p[0], out var date))
+                        currentHolidays.Add(Holiday.Create(date, p[1]));
+                }
+            }
+            lblHolidaysUsed.Text = $"Holidays Used: {currentHolidays.Count}";
+            lbHolidaysUsed.Items.Clear();
+            lbHolidaysUsed.DisplayMember = "Display";
+            lbHolidaysUsed.Items.AddRange(currentHolidays.ToArray());
+        }
+
+        private void LoadHolidaysFromFile()
+        {
+            tbHolydays.Clear();
+            if (File.Exists("ledigadagar.csv"))
+            {
+                var days = File.ReadAllLines("ledigadagar.csv");
+                foreach (var day in days)
+                {
+                    if (!string.IsNullOrEmpty(day))
+                    {
+                        var ds = day.Split('\t');
+                        tbHolydays.AppendText($"{ds[0]} {ds[1]}\r\n");
+                    }
+                }
+            }
+        }
+
+        private List<DateRange> GeneratePeriods(int year)
+        {
+            //2024-12-21 - 2025-01-20
+            //2025-01-21 - 2025-02-20
+            //2025-02-21 - 2025-03-20
+            var periods = new List<DateRange>();
+            periods.Add(DateRange.Create(year - 1, 12, 21, year, 01, 20));
+            periods.Add(DateRange.Create(year, 01, 21, year, 02, 20));
+            periods.Add(DateRange.Create(year, 02, 21, year, 03, 20));
+            periods.Add(DateRange.Create(year, 03, 21, year, 04, 20));
+            periods.Add(DateRange.Create(year, 04, 21, year, 05, 20));
+            periods.Add(DateRange.Create(year, 05, 21, year, 06, 20));
+            periods.Add(DateRange.Create(year, 06, 21, year, 07, 20));
+            periods.Add(DateRange.Create(year, 07, 21, year, 08, 20));
+            periods.Add(DateRange.Create(year, 08, 21, year, 09, 20));
+            periods.Add(DateRange.Create(year, 09, 21, year, 10, 20));
+            periods.Add(DateRange.Create(year, 10, 21, year, 11, 20));
+            periods.Add(DateRange.Create(year, 11, 21, year, 12, 20));
+
+            return periods;
+        }
+        private int CalculateForPeriod(DateOnly start, DateOnly end, bool report = true)
+        {
+            if (report)
+            {
+                rtpOutput.Clear();
+                rtpOutput.AppendText($"Calculation period: {start} - {end}\n");
+            }
+
+            int totalHours = 0; // Counter for total hours
+            int currentWeek = 0;
+            int currentMonth = start.Month;
+            // Iterate from start to end
+            for (var date = start; date <= end; date = date.AddDays(1))
+            {
+                if (report && currentMonth != date.Month)
+                {
+                    rtpOutput.AppendText($"\t\t100:00:00\t\t{date.ToString("MMMM").ToUpper()}\n");
+                    currentMonth = date.Month;
+                }
+
+                bool isWeekend = date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday;
+                string star = isWeekend ? "*" : "";
+
+                var holiday = currentHolidays.FirstOrDefault(h => h.Date == date);
+                bool isHoliday = holiday != null && cbHolidaysUsed.Checked;
+
+                if (!isWeekend && !isHoliday)
+                {
+                    totalHours += 8;
+                }
+
+                if (report)
+                {
+                    if (isWeekend && !cbShowWeekends.Checked)
+                        continue;
+
+                    var cwn = ISOWeek.GetWeekOfYear(date.ToDateTime(TimeOnly.MinValue));
+                    var weekToShow = string.Empty;
+                    if (cwn != currentWeek)
+                    {
+                        currentWeek = cwn;
+                        weekToShow = cwn.ToString();
+                    }
+                    else
+                    {
+                        weekToShow = string.Empty;
+                    }
+
+                    var holidayString = isHoliday ? holiday.Name : string.Empty;
+
+                    var timeString = isHoliday ? "00:00:00" : "08:00:00";
+                    rtpOutput.AppendText($"{weekToShow}\t{date}\t{timeString}\t{date.DayOfWeek} {star}{holidayString}\n");
+                }
+            }
+            if (report)
+            {
+                rtpOutput.AppendText($"Total hours for period: {totalHours}\n");
+            }
+            return totalHours;
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            rtpOutput.Clear();
+            Form1_Load(sender, e);
+        }
+    }
+
+    [DebuggerDisplay("{Display}")]
+    public class DateRange
+    {
+        public DateOnly From { get; set; }
+        public DateOnly To { get; set; }
+        public string Display => $"{From} - {To}";
+
+        public static DateRange Create(int fyear, int fmonth, int fday, int tyear, int tmonth, int tday)
+        {
+            return new DateRange
+            {
+                From = new DateOnly(fyear, fmonth, fday),
+                To = new DateOnly(tyear, tmonth, tday)
+            };
+        }
+    }
+
+    [DebuggerDisplay("{Display}")]
+    public class Holiday
+    {
+        public DateOnly Date { get; set; }
+        public string Name { get; set; }
+        public string Display => $"{Date} {Name}";
+        public static Holiday Create(DateOnly date, string name)
+        {
+            return new Holiday
+            {
+                Date = date,
+                Name = name
+            };
+        }
+    }
+}
